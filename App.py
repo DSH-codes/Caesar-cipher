3820b31b1454f10f840c84c6a6b6b04706855ff7
Skovand <Skovand@proton.me>
2025-02-19 02:22:08 +0300

    version 0.1

--- App.py
+++ App.py
@@ -1,7 +1,24 @@
 
 import customtkinter as ui
 
-# soon
+
+
+
+class App(ui.CTk):
+
+    def __init__(self, geometry="400x600"):
+        super().__init__()
+        self.geometry(geometry)
+        self.resizable(0, 0)
+        self.title(f"{' ' * 42}CaesarCipher 0.1")
+
+
+
+
+if __name__ == "__main__":
+
+    app = App()
+    app.mainloop()
 
 
 
