7d507bf94546fd633131dc6d6f308e820a648b64
Skovand <Skovand@proton.me>
2025-02-25 04:32:30 +0300

    the encode function works

--- App.py
+++ App.py
@@ -1,59 +1,50 @@
 
 import customtkinter as ui
 
-import string as st
-
 import functions
 
-# ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
 
 
 class App(ui.CTk):
+    button_padding_y = 10  # vertical
+    button_padding_x = 15  # horizontal
 
-    button_padding_y = 10    # vertical
-    button_padding_x = 15    # horizontal
-
-    def __init__(self, geometry = "400x600"):
-
+    def __init__(self, geometry="400x600"):
         super().__init__()
         self.geometry(geometry)
         self.resizable(True, True)
         self.title(f"{' ' * 42}CaesarCipher 0.2")
 
         self.entry = ui.CTkEntry(self)
-        self.entry.pack(pady = 30)
+        self.entry.pack(pady=30)
 
-        self.window = ui.CTkTextbox(self, width = 350, height = 350, corner_radius = 15)
+        self.window = ui.CTkTextbox(self, width=350, height=350, corner_radius=15)
         self.window.pack()
 
-        self.frame_ = ui.CTkFrame(self, fg_color = self.cget("fg_color"), height = 150, width = 350)
+        self.frame_ = ui.CTkFrame(self, fg_color=self.cget("fg_color"), height=150, width=350)
         self.frame_.pack()
 
-        self.import_ = ui.CTkButton(self.frame_, text = "Import")
-        self.import_.grid(row = 0, column = 0, pady = self.button_padding_y, padx = self.button_padding_x)
+        self.import_ = ui.CTkButton(self.frame_, text="Import")
+        self.import_.grid(row=0, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
-        self.encode_ = ui.CTkButton(self.frame_, text = "Encode")
-        self.encode_.grid(row = 0, column = 1, pady = self.button_padding_y, padx = self.button_padding_x)
+        self.encode_ = ui.CTkButton(self.frame_, text="Encode")
+        self.encode_.grid(row=0, column=1, pady=self.button_padding_y, padx=self.button_padding_x)
 
-        self.save_as = ui.CTkButton(self.frame_, text = "Save as")
-        self.save_as.grid(row = 1, column = 0, pady = self.button_padding_y, padx = self.button_padding_x)
+        self.save_as = ui.CTkButton(self.frame_, text="Save as")
+        self.save_as.grid(row=1, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
-        self.decode = ui.CTkButton(self.frame_, text ="Decode")
-        self.decode.grid(row = 1, column = 1, pady = self.button_padding_y, padx = self.button_padding_x)
+        self.decode = ui.CTkButton(self.frame_, text="Decode")
+        self.decode.grid(row=1, column=1, pady=self.button_padding_y, padx=self.button_padding_x)
 
         self.help = ui.CTkButton(self.frame_, text="Help")
-        self.help.grid(row = 2, column = 0, pady=self.button_padding_y, padx=self.button_padding_x)
+        self.help.grid(row=2, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
-        self.clear_window = ui.CTkButton(self.frame_, text="Clear", command = lambda: functions.cleaner(self.window))
-        self.clear_window.grid(row = 2, column = 1, pady = self.button_padding_y, padx=self.button_padding_x)
-
-
-
-
-
+        self.clear_window = ui.CTkButton(self.frame_, text="Clear", command=lambda: functions.cleaner(self.window))
+        self.clear_window.grid(row=2, column=1, pady=self.button_padding_y, padx=self.button_padding_x)
 
 
 if __name__ == "__main__":
-
     app = App()
     app.mainloop()
--- functions.py
+++ functions.py
@@ -1,10 +1,18 @@
 
 import customtkinter as ct
+
 import string as st
 
 
-def cleaner(widget: ct.CTkTextbox):
-    """Deletes content of the Textbox widget"""
+
+
+def cleaner(widget: ct.CTkTextbox) -> None:
+    """Deletes content of the Textbox widget
+
+    Parameters:
+        widget(CTk.Textbox)
+
+    """
 
     widget.delete(0.0, ct.END)
 
@@ -16,45 +24,40 @@ def encode(message: str, shift = 1) -> str:
 
     Parameters:
         message(str): a string to be encoded
-        shift(int): a shift of alphabet
+        shift(int): a shift of the alphabet
 
     Returns:
-        str: encoded message as a string value
+        converted(str): encoded message as a string value
 
     Examples:
-        > encode("Hello", 3)
+        >>> encode("Hello", 3)
+        'Khoor'
+        >>> encode("London", 4)
+        'Psrhsr'
+        >>> encode("Hello world!", 2)
+        'Jgnnq yqtnf!'
+        >>> encode("I am 25 years old", 2)
+        'K co 25 agctu qnf'
 
-        > encode("London", 4)
-
-        > encode("Hello world!", 2)
     """
 
 
 
-    ab = [i for i in st.ascii_lowercase]  # stands for alphabet, content = lowercase english alphabet
-    abu = [i for i in st.ascii_uppercase]  # the same as above but uppercase
+    al = [i for i in st.ascii_lowercase]  # EN alphabet lowercase
+    au = [i for i in st.ascii_uppercase]  # EN alphabet uppercase
+
+    # the formula for encoding is: (alphabet.index(char) + shift) % len(alphabet)
+
+    # The comprehension below is too complicated? This is because of this ternary -> (al if i.islower() else au) <-  marked by dashes
+    # This version with no ternary, but works only with LOWERCASE messages: converted = [al[(al.index(i) + shift) % len(al)] if i in al and i != " " else i for i in message.lower()]
+    # This version with no ternary, but works only with UPPERCASE messages: converted = [au[(au.index(i) + shift) % len(au)] if i in au and i != " " else i for i in message.lower()]
 
 
-
-    # the formula is: (alphabet.index(char) + shift) % len(alphabet)
-    converted = [ab[(ab.index(i) + shift) % len(ab)] if i in ab and i != " " else i for i in message.lower()]
+    #            --------------------------    -------------------------                                        -------------------------
+    converted = [(al if i.islower() else au)[((al if i.islower() else au).index(i) + shift) % len(al)] if i in (al if i.islower() else au) and i != " " else i for i in message]
     converted = "".join(converted)
 
     return converted
-
-
-print(encode("zzz!", 2))
-
-
-
-
-
-
-
-
-
-
-
 
 
 
