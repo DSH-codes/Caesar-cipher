0b7b6c47ce0101f01d3c1f5a5684f9e5f1819410
Skovand <Skovand@proton.me>
2025-03-07 04:28:09 +0300

    save function

--- App.py
+++ App.py
@@ -1,7 +1,14 @@
+import tkinter.messagebox
 
 import customtkinter as ui
 
 import functions as fcs
+
+from tkinter import messagebox as mb
+
+from tkinter import filedialog as fd
+
+
 
 
 # Giving the command value, to widgets, trough configure, is done for readability
@@ -33,15 +40,15 @@ class App(ui.CTk):
         self.frame_.pack()
 
         self.import_ = ui.CTkButton(self.frame_, text="Import")
-        self.import_.configure(command=lambda: fcs.insert_imported_text(self.original_textbox, fcs.import_text, fcs.clear, self.original_textbox, self.encoded_textbox))
+        self.import_.configure(command=self._import_text)
         self.import_.grid(row=0, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
         self.encode_ = ui.CTkButton(self.frame_, text="Encode")
-        args = (self.original_textbox, self.entry, fcs.encoder, fcs.convert_lines, self.encoded_textbox)
-        self.encode_.configure(command = lambda: fcs.show_secret(*args))
+        self.encode_.configure(command = self._encode_and_show)
         self.encode_.grid(row=0, column=1, pady=self.button_padding_y, padx=self.button_padding_x)
 
         self.save_as = ui.CTkButton(self.frame_, text="Save as")
+        self.save_as.configure(command = self._save)
         self.save_as.grid(row=1, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
         self.decode = ui.CTkButton(self.frame_, text="Decode")
@@ -51,25 +58,85 @@ class App(ui.CTk):
         self.help.grid(row=2, column=0, pady=self.button_padding_y, padx=self.button_padding_x)
 
         self.clear_window = ui.CTkButton(self.frame_, text="Clear")
-        self.clear_window.configure(command=self.clear_textboxes)
+        self.clear_window.configure(command=self._clear_textboxes)
         self.clear_window.grid(row=2, column=1, pady=self.button_padding_y, padx=self.button_padding_x)
 
-    def clear_textboxes(self):
+    def _encode_and_show(self):
+        """Gets a text from the upper textbox
+        encodes it, the inserts into the textbox
+        below"""
 
-        """Clears the two text boxes"""
+        shift = self.entry.get()
+        shift = int(shift) if shift else 1
+
+        encoded_message = fcs.convert_lines(self.original_textbox, fcs.encoder, shift)
+
+        self._clear_textboxes(False)
+
+        self.encoded_textbox.configure(state = "normal")
+        self.encoded_textbox.insert(0.0, encoded_message)
+        self.encoded_textbox.configure(state = "disabled")
+
+
+    def _clear_textboxes(self, both = True):
+
+        """Clears one or both
+        of the textboxes"""
 
         # Just clears the first textbox
         # Then do the same for the second one
 
-        self.original_textbox.delete(0.0, "end")
+        self.encoded_textbox.configure(state="normal")
+        self.encoded_textbox.delete(0.0, "end")
+        self.encoded_textbox.configure(state="disabled")
+
+        if not both:   # if not both, return
+            return
 
         # Doing it this way, because ->
         # if widget["state"] = "disabled"
         # <- is not available
 
-        self.encoded_textbox.configure(state = "normal")
-        self.encoded_textbox.delete(0.0, "end")
-        self.encoded_textbox.configure(state = "disabled")
+        self.original_textbox.delete(0.0, "end")
+
+
+    def _import_text(self):
+        """Takes a text from txt file
+        and inserts it into the upper
+        textbox for encoding"""
+
+        text = fcs.import_text()
+
+        self._clear_textboxes()
+
+        self.original_textbox.insert(0.0, text)
+
+
+    def _save(self):
+        """Saves original, encoded or
+        both messages as a txt file."""
+
+        original_message = self.original_textbox.get(0.0, "end")
+        original_message = "no message was written\n" if original_message == "\n" else original_message
+
+        # if no text in the textboxes, default values to be given
+
+        encoded_message = self.encoded_textbox.get(0.0, "end")
+        encoded_message = "no message was encoded" if encoded_message == "\n" else encoded_message
+
+
+        s = fd.asksaveasfilename(defaultextension = ".txt", filetypes = [("Text file", "*.txt")])
+
+
+        with open(s, "w") as file:
+            file.write(original_message)
+            file.write(f"\n{('-' * 45)}\n\n")
+            file.write(encoded_message)
+
+
+
+
+
 
 
 
--- functions.py
+++ functions.py
@@ -8,75 +8,7 @@ from typing import Callable, Union
 from tkinter import filedialog as fd
 
 
-# ---------------------------------------------WIDGETS OPERATING SECTION------------------------------------------------
-
-def clear(*widgets: ct.CTkTextbox) -> None:
-    """Deletes content of the Textbox widgets
-
-    Parameters:
-        widgets(CTk.Textbox): a Textbox widget
-        to be cleared
-    """
-
-    # Just clears the first textbox
-    # Then do the same for the second one
-
-    widgets[0].delete(0.0, "end")
-
-    # Doing it this way, because ->
-    # if widget["state"] = "disabled"
-    # <- is not available
-
-    widgets[1].configure(state = "normal")
-    widgets[1].delete(0.0, "end")
-    widgets[1].configure(state = "disabled")
-
-
-def show_secret(*args: Union[ct.CTkTextbox, ct.CTkEntry, Callable, Callable, ct.CTkTextbox]) -> None:
-    """
-    Takes a message from a textbox, encodes it
-    and inserts it into another textbox
-
-    Parameters:
-        args[0]: a textbox widget to get a text from
-        args[1]: an entry widget to get a shift from
-        args[2]: a word encoding function to encrypt words using the given shift
-        args[3]: a function to apply the word encoding func to every word in the text, and an give encrypted text
-        args[4]: a textbox widget into which, encrypted text will be inserted
-
-
-    What is going on here:
-
-        This SHOW_SECRET function, takes a text from args[0] which is a Textbox widget.
-        Then takes value from args[1] which an Entry widget and coverts it into int, it will be the shift value
-        Then calls args[3] CONVERT_LINES function, passing to it args[2] ENCODER function along with the shift
-        value and assigning the call to ENCRYPTED_MESSAGE variable
-        Then ENCRYPTED_MESSAGE is inserted into args[4] which is a Textbox widget
-    """
-
-    shift = args[1].get()
-    shift = int(shift) if shift else 1          # default is 1, if args[1].get() == " "
-
-    encrypted_message = args[3](args[0], args[2], shift)
-
-    target_widget = args[4]
-
-    target_widget.configure(state = "normal")
-    target_widget.delete(0.0, "end")            # delete the previous text, to avoid repetitive insertions
-    target_widget.insert(0.0, encrypted_message)
-    target_widget.configure(state = "disabled")
-
-
-def insert_imported_text(to_where: ct.CTkTextbox, importing_function, clearing_function, *clear_them):
-
-    clearing_function(*clear_them)
-
-    text = importing_function()
-
-    to_where.insert(0.0, text)
-
-
-# ---------------------------------------------RETURNING FUNCTIONS SECTIONS---------------------------------------------
+# ---------------------------------------ONLY RETURNING FUNCTIONS SECTIONS HERE-----------------------------------------
 
 
 def encoder(message: str, shift) -> str:
